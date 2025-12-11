1e7e95187e684e5e2329cc3f4402c01cc5630ddc
HellWatcher <deathlord_seiged@hotmail.com>
2025-12-11 12:20:14 -0500

    added models package

---
+++ models/__init__.py
@@ -0,0 +1,10 @@
+"""Models package initializer.
+
+Mark `models` as a package so imports and type-checking resolve to
+`models.*` module names consistently.
+"""
+
+__all__ = [
+    "food",
+    "plan",
+]
